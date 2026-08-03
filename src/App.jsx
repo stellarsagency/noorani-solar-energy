7b76d1c4d1c676a0412f8ed55ef4f3e147eda98e
stellarsagency <stellarsagency@users.noreply.github.com>
2026-08-03 06:57:56 -0700

    Switch to HashRouter for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+﻿import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import PlaceholderPage from './components/PlaceholderPage'
 import Home from './pages/Home'
@@ -11,7 +11,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
@@ -24,7 +24,7 @@ function App() {
           <Route path="*" element={<PlaceholderPage title="Page Not Found" />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
