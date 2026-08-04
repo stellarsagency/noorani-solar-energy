81f1b8371a03d712eec0f40c9a4020969b9db85b
stellarsagency <stellarsagency@users.noreply.github.com>
2026-08-04 05:56:20 -0700

    Slightly larger centered mobile logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
           <img
             src={logo}
             alt="ElecSun Solar Energy logo"
-            className="hero-anim h-20 w-auto max-w-[14rem] object-contain sm:h-20 sm:max-w-[14rem] lg:h-32 lg:max-w-[20rem]"
+            className="hero-anim h-24 w-auto max-w-[15rem] object-contain sm:h-24 sm:max-w-[15rem] lg:h-32 lg:max-w-[20rem]"
           />
         </Link>
 
