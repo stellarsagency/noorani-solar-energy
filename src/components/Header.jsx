d5d60b03139a43768229791ead6e15e4713fae0d
stellarsagency <stellarsagency@users.noreply.github.com>
2026-08-04 05:34:01 -0700

    Increase mobile logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
           <img
             src={logo}
             alt="ElecSun Solar Energy logo"
-            className="hero-anim h-24 w-auto max-w-[16rem] object-contain sm:h-24 sm:max-w-[18rem] lg:h-32 lg:max-w-[21rem]"
+            className="hero-anim h-28 w-auto max-w-[18rem] object-contain sm:h-24 sm:max-w-[18rem] lg:h-32 lg:max-w-[21rem]"
           />
         </Link>
 
